ac153963b4144c9802a636c9da72c799da6e8577
natasha <natasha08@me.com>
2016-09-27 09:45:02 -0600

    update egym page w/ btn

--- spec/pages/egym_spec.js
+++ spec/pages/egym_spec.js
@@ -1,9 +1,21 @@
 import { mount } from 'enzyme';
 import Egym from 'pages/egym';
+import egymRepository from 'repositories/egym';
 
 describe('The Egym Page', function() {
   it('renders layout children', function() {
     let wrapper = mount(<Egym />);
     expect(wrapper).to.have.length(1);
+  });
+  context('workouts', function() {
+    let wrapper = mount(<Egym />);
+    it('gets workouts', function() {
+      const workoutStub = this.sinon.stub(egymRepository, 'fetch').returns(new Promise(_.noop));
+      const btn = wrapper.find('button.btn-fetch');
+      btn.simulate('click');
+
+      expect(wrapper.state().errors).deep.equal([]);
+      expect(workoutStub).to.have.been.calledOnce;
+    });
   });
 });
--- src/pages/egym.js
+++ src/pages/egym.js
@@ -1,7 +1,18 @@
+import egymRepository from 'repositories/egym';
+
 export default React.createClass({
+  getInitialState: function() {
+    return {
+      errors: []
+    }
+  },
+  fetch: function() {
+    egymRepository.fetch()
+  },
   render: function() {
     return(
       <div className="wrapper">
+        <button className="btn-fetch" onClick={ this.fetch }>Workouts</button>
       </div>
     );
   }
