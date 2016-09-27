b075fcdfe85e34f2f77fe5e43b2c32aa4b1d93be
natasha <natasha08@me.com>
2016-09-27 15:32:36 -0600

    WIP efridge form

--- spec/pages/efridge_spec.js
+++ spec/pages/efridge_spec.js
@@ -23,6 +23,23 @@ describe.only('The efridge Page', function() {
     it('submits the form', function() {
       const foodStub = this.sinon.stub(efridgRepository, 'post').returns(new Promise(_.noop));
       //grab info from the schema and fill in b4 click;
+      const food_name = wrapper.find('input.food_name');
+      const brand = wrapper.find('input.brand');
+      const serving_size = wrapper.find('input.serving_size');
+      const total_calories = wrapper.find('input.total_calories');
+      const fat_grams = wrapper.find('input.fat_grams');
+      const carbohydrate_grams = wrapper.find('input.carbohydrate_grams');
+      const protein_grams = wrapper.find('input.protein_grams');
+      const total_grams = wrapper.find('input.total_grams');
+
+      food_name.get(0).value = 'Banana';
+      brand.get(0).value = 'Chiquata';
+      serving_size.get(0).value = 1;
+      total_calories.get(0).value = 80;
+      fat_grams.get(0).value = 0;
+      carbohydrate_grams.get(0).value = 32;
+      protein_grams.get(0).value = 0;
+      total_grams.get(0).value = 32;
 
       const btn = wrapper.find('button.btn-post');
       btn.simulate('click');
--- src/pages/efridge.js
+++ src/pages/efridge.js
@@ -2,6 +2,7 @@ import efridgRepository from 'repositories/efridge';
 import store from 'store';
 import efridgeActions from 'actions/efridge';
 
+let title = 'Your Efridge';
 export default React.createClass({
   getInitialState: function() {
     return {
@@ -11,17 +12,70 @@ export default React.createClass({
   fetch: function() {
     efridgRepository.fetch()
   },
-  dispatch: function() {
-    const food_items = {
-      food_name: 'banana'
-    }
-    this.setState({ food_items });
-    store.dispatch(efridgeActions.fetch(food_items));
+  submit: function() {
+    const food_name = this.refs.food_name.value;
+    const brand = this.refs.brand.value;
+    const serving_size = this.refs.serving_size.value;
+    const total_calories = this.refs.total_calories.value;
+    const fat_grams = this.refs.fat_grams.value;
+    const carbohydrate_grams = this.refs.carbohydrate_grams.value;
+    const protein_grams = this.refs.protein_grams.value;
+    const total_grams = this.refs.total_grams.value;
+
+    const efridge = {
+      food_name, brand, serving_size, total_calories, fat_grams,
+      carbohydrate_grams, protein_grams, total_grams
+     };
+
+      efridgRepository.post({ efridge });
   },
   render: function() {
     return(
       <div className="wrapper">
        <button className='btn-fetch' onClick={ this.fetch }>Food Items</button>
+       <div className='wrapper'>
+         <div className='form'>
+           <h1>{ title }</h1>
+           <div className='form-section'>
+             <label className='form-label'>Name of Food</label>
+             <input className='food_name' ref='food_name' />
+           </div>
+           <div className='form-section'>
+             <label className='form-label'>Brand</label>
+             <input className='brand' ref= 'brand' />
+           </div>
+           <div className='form-section'>
+             <label className='form-label'>Serving Size</label>
+             <input className='serving_size' ref='serving_size' />
+           </div>
+           <div className='form-section'>
+             <label className='form-label'>Total Calories</label>
+             <input className='total_calories' ref= 'total_calories' />
+           </div>
+           <div className='form-section'>
+             <label className='form-label'>Fat Grams</label>
+             <input className='fat_grams' ref='fat_grams' />
+           </div>
+           <div className='form-section'>
+             <label className='form-label'>Carbohydrate Grams</label>
+             <input className='carbohydrate_grams' ref= 'carbohydrate_grams' />
+           </div>
+           <div className='form-section'>
+             <label className='form-label'>Protein Grams</label>
+             <input className='protein_grams' ref='protein_grams' />
+           </div>
+           <div className='form-section'>
+             <label className='form-label'>Total Grams</label>
+             <input className='total_grams' ref= 'total_grams' />
+           </div>
+           <div className='form-submit'>
+             <button className='btn-post' onClick={ this.submit }>Submit</button>
+           </div>
+           <div className='form-section'>
+             <div className='form-errors'>{ this.state.errors }</div>
+           </div>
+         </div>
+       </div>
       </div>
     );
   }
